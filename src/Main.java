4bbda58f6e2dcdce826aef6b0268b97eec947da2
SebastianCal <sebastiancalcotorres38@gmail.com>
2023-02-17 19:28:07 -0500

    comienzo de ejercicio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bienvenido");
+        System.out.println("Catalogo Tienda TEC");
+        System.out.println("1. Mouse => 45.000");
+        System.out.println("1. Mouse => 45.000");
+        System.out.println("2. Teclados => 60.000");
+        System.out.println("3. Monitores => 950.000");
+        System.out.println("4. Ram => 125.000");
+        System.out.println("5. Disco Duro => 280.000");
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+        System.out.println("Digite el numero de la pieza que decea: ");
+        numero = teclado.nextInt();
     }
+
 }
